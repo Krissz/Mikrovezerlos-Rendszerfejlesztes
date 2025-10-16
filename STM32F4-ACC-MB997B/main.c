92c60b6f2b20438179489a24e6ed60f7e3e4681a
Krisztián Zsupányi <zsupi1987@gmail.com>
2025-10-16 23:29:51 +0200

    MB997B, SPI fix

--- STM32F4-ACC-MB997B/main.c
+++ STM32F4-ACC-MB997B/main.c
@@ -18,13 +18,16 @@ static int32_t accraw[LIS302DL_ACC_NUMBER_OF_AXES];
 static float acccooked[LIS302DL_ACC_NUMBER_OF_AXES];
 static char axisID[LIS302DL_ACC_NUMBER_OF_AXES] = {'X', 'Y', 'Z'};
 
+
 static const SPIConfig spicfg = {
-  FALSE,
-  NULL,
-  GPIOE,
-  GPIOE_CS_SPI,
-  SPI_CR1_BR_0 | SPI_CR1_CPOL | SPI_CR1_CPHA,
-  0
+  .circular         = false,
+  .slave            = false,
+  .data_cb          = NULL,
+  .error_cb         = NULL,
+  .ssport           = GPIOE,
+  .sspad            = GPIOE_CS_SPI,
+  .cr1              = SPI_CR1_BR_0 | SPI_CR1_CPOL | SPI_CR1_CPHA,
+  .cr2              = 0U
 };
 
 static LIS302DLConfig lis302dlcfg = {
@@ -54,17 +57,17 @@ static THD_FUNCTION(AccThd, arg) {
   while (true) {
 
     lis302dlAccelerometerReadRaw(&LIS302DLD1, accraw);
-    chprintf(smShellGetHandler(SHELL_INTERFACE_CDC), "LIS302DLD1 Raw: ");
+    chprintf(smShellGetHandler(SHELL_INTERFACE_UART), "LIS302DLD1 Raw: ");
     for(uint8_t i = 0; i < LIS302DL_ACC_NUMBER_OF_AXES; i++) {
-      chprintf(smShellGetHandler(SHELL_INTERFACE_CDC), "%c: %d ", axisID[i], accraw[i]);
+      chprintf(smShellGetHandler(SHELL_INTERFACE_UART), "%c: %d ", axisID[i], accraw[i]);
     }
 
     lis302dlAccelerometerReadCooked(&LIS302DLD1, acccooked);
-    chprintf(smShellGetHandler(SHELL_INTERFACE_CDC), " Cooked: ");
+    chprintf(smShellGetHandler(SHELL_INTERFACE_UART), " Cooked: ");
     for(uint8_t i = 0; i < LIS302DL_ACC_NUMBER_OF_AXES; i++) {
-      chprintf(smShellGetHandler(SHELL_INTERFACE_CDC), "%c: %.3f ", axisID[i], acccooked[i]/1000);
+      chprintf(smShellGetHandler(SHELL_INTERFACE_UART), "%c: %.3f ", axisID[i], acccooked[i]/1000);
     }
-    chprintf(smShellGetHandler(SHELL_INTERFACE_CDC),"\r\n");
+    chprintf(smShellGetHandler(SHELL_INTERFACE_UART),"\r\n");
 
     chThdSleepMilliseconds(100);
   }
@@ -78,13 +81,13 @@ int main(void) {
   chSysInit();
 
   // Initialize shell with desired interface (e.g., UART or CDC)
-  smShellInitInterface(SHELL_INTERFACE_CDC); // Change to SHELL_INTERFACE_CDC for CDC
+  smShellInitInterface(SHELL_INTERFACE_UART); // Change to SHELL_INTERFACE_CDC for CDC
 
   /* Creates the Acc thread.*/
   chThdCreateStatic(waAccThd, sizeof(waAccThd), NORMALPRIO + 1, AccThd, NULL);
 
   while (true) {
-    smShellKeepAlive(SHELL_INTERFACE_CDC); // Change to SHELL_INTERFACE_CDC for CDC
+    smShellKeepAlive(SHELL_INTERFACE_UART); // Change to SHELL_INTERFACE_CDC for CDC
     chThdSleepMilliseconds(500);
   }
 }
